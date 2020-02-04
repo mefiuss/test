ee7a5eba848995c9548676e7794ac45c4c1531b0
mefius5 <mefius170@op.pl>
2020-02-04 14:38:47 +0100

    index2 created

---
+++ index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<<<<<<< HEAD
+    <title>Header</title>
+=======
+    <title>Document</title>
+>>>>>>> parent of 79edc5d... removed
+</head>
+<body>
+    
+</body>
+</html>
